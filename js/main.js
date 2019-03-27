4a761d7c57e2d1d2a8265bf2c4c865fdea053dbb
Myles Bennett <myles.bennett@hotmail.com>
2019-03-27 15:51:26 -0400

    added functionality for turning lights on/off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+const lightSwitch = document.querySelector('.switch');
+const body = document.querySelector('body');
+const status = document.querySelector('.status');
 
-
+//Turning switch on and off
+lightSwitch.addEventListener('click', function() {
+    if (lightSwitch.classList.contains('on')) {
+        lightSwitch.classList.remove("on");
+        lightSwitch.className += " off"; 
+        body.className = "dark"; 
+        status.innerText = "Hey, who turned off the lights?";
+    } else if (lightSwitch.classList.contains('off')) {
+        lightSwitch.classList.remove("off");
+        lightSwitch.className += " on";
+        body.className = "light";
+        status.innerText = "It's so bright in here!"
+    }
+});
